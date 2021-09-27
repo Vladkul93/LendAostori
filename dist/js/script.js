d21ac69b2b30638ce9d202e01a07dc138dcf3a7f
кул <Limp277@gmail.com>
2021-09-27 11:35:58 +0300

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,25 @@
+// let sidenav = document.querySelector('.sidenav');
+// function openNav() {
+//     sidenav.classList.toggle('active');
+//     }
+    
+//     function closeNav() {
+//         sidenav.classList.toggle('active');
+//     }
+
+    
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("mySidenav").style.left = "0%";
+    }
+    
+
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("mySidenav").style.left = "-10%";
+    }
+
+
+
+
+
